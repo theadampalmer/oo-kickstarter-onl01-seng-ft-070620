9e715aaca8abc8f9dd31c3ef0052970cd9873bd8
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 22:16:44 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer 
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
